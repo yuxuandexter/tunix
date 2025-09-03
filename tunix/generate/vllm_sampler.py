5f178089175fc19f661446882f5b02084f2e60d6
Lin Chai <linchai@google.com>
2025-09-03 13:58:13 -0700

    [Tunix] bring back the fix for new llm.generate API which got reverted due to the integration merge.
    PiperOrigin-RevId: 802696704

--- tunix/generate/vllm_sampler.py
+++ tunix/generate/vllm_sampler.py
@@ -28,6 +28,7 @@ from tunix.generate import utils
 import tunix.generate.tokenizer_adapter as tok_adapter
 from tunix.rl import reshard
 from vllm import LLM
+from vllm.inputs import TokensPrompt
 from vllm.outputs import RequestOutput
 
 
@@ -252,7 +253,7 @@ class VllmSampler(base_sampler.BaseSampler):  # pylint: disable=invalid-name
 
     prompt_ids = [self.tokenize(x) for x in input_strings]
     outputs = self.llm.generate(
-        prompts=None,
+        prompts=[TokensPrompt(prompt_token_ids=ids) for ids in prompt_ids],
         prompt_token_ids=prompt_ids,
         sampling_params=self.sampling_params,
         use_tqdm=True,
